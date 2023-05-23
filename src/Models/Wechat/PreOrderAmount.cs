d9928143187930c23fcfa89789e71c71c7fd870f
何明钰 <hmy4677@qq.com>
2023-05-23 19:53:08 +0800

    add nuget

--- src/Models/Wechat/PreOrderAmount.cs
+++ src/Models/Wechat/PreOrderAmount.cs
@@ -3,18 +3,18 @@
 /// <summary>
 /// 预付订单金额.
 /// </summary>
-internal class PreOrderAmount
+public class PreOrderAmount
 {
     /// <summary>
     /// 货币类型.
     /// </summary>
     [JsonProperty("currency")]
-    internal string Currency { get; set; } = "CNY";
+    public string Currency { get; set; } = "CNY";
 
     /// <summary>
     /// 总金额(单位:分).
     /// </summary>
     [Required]
     [JsonProperty("total")]
-    internal int Total { get; set; }
+    public int Total { get; set; }
 }
--- src/Models/Wechat/PrepayRequest.cs
+++ src/Models/Wechat/PrepayRequest.cs
@@ -8,35 +8,35 @@ internal class PrepayRequest
     /// <summary>
     /// 应用id.
     /// </summary>
-    internal string appid { get; set; }
+    internal string appid { get; set; } = string.Empty;
 
     /// <summary>
     /// 直连商户号.
     /// </summary>
-    internal string mchid { get; set; }
+    internal string mchid { get; set; } = string.Empty;
 
     /// <summary>
     /// 商品描述.
     /// </summary>
-    internal string description { get; set; }
+    internal string description { get; set; } = string.Empty;
 
     /// <summary>
     /// 商户订单号.
     /// </summary>
-    internal string out_trade_no { get; set; }
+    internal string out_trade_no { get; set; } = string.Empty;
 
     /// <summary>
     /// 通知地址.
     /// </summary>
-    internal string notify_url { get; set; }
+    internal string notify_url { get; set; } = string.Empty;
 
     /// <summary>
     /// 订单金额信息.
     /// </summary>
-    internal PreOrderAmount amount { get; set; }
+    internal PreOrderAmount amount { get; set; } = new PreOrderAmount();
 
     /// <summary>
     /// 支付者信息.
     /// </summary>
-    internal PayerInfo payer { get; set; }
+    internal PayerInfo payer { get; set; } = new PayerInfo();
 }
